d7507d4235c6d0af31c59e8275ce7e6a5a3fe626
ericmuh <muhwezi1000@gmail.com>
2022-12-18 17:01:03 +0300

    whitenoise

--- project/settings.py
+++ project/settings.py
@@ -120,11 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = "static/"
+
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
