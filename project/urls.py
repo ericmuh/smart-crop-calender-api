ecb9d19aaeef1e093231a5b5e9cb072973ad3c92
ericmuh <muhwezi1000@gmail.com>
2022-12-18 17:35:28 +0300

    whitenoise

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+
 # from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,22 +11,21 @@ from drf_yasg import openapi
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="SmartcropCalender API",
-      default_version='v1',
-      description="Api for the SmartcropCalender App",
-      terms_of_service="https://www.SmartcropCalender.com/policies/terms/",
-      contact=openapi.Contact(email="muhwezi1000@gmail.com"),
-      license=openapi.License(name="UnLicensed"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="SmartcropCalender API",
+        default_version="v2",
+        description="Api for the SmartcropCalender App",
+        terms_of_service="https://www.SmartcropCalender.com/policies/terms/",
+        contact=openapi.Contact(email="muhwezi1000@gmail.com"),
+        license=openapi.License(name="UnLicensed"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/crops/", include("crop.urls")),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-admin.site.site_header = 'SmartcropCalender Administration'
+admin.site.site_header = "SmartcropCalender Administration"
